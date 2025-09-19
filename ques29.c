af5d4200576b470855467ab242278aac1cce8de8
priyanshu50901 <priyanshu865038725@gmail.com>
2025-09-19 12:52:27 +0530

    Add files via upload

---
+++ ques29.c
@@ -0,0 +1,28 @@
+//Q29: Write a program to calculate the factorial of a number.
+
+/*
+Sample Test Cases:
+Input 1:
+5
+Output 1:
+120
+
+Input 2:
+3
+Output 2:
+6
+
+*/
+#include <stdio.h>
+
+int main() {
+    int n;
+    long  factorial = 1;
+printf("enter a number");
+    scanf("%d", &n);
+    for (int i = 2; i <= n; i++) {
+        factorial *= i;
+    }
+    printf("%lld\n", factorial);
+    return 0;
+}
